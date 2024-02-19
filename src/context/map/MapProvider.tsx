19dcae9255fd092491be095719462d0627319a76
Diego Pacori <dpacoria@unsa.edu.pe>
2024-02-18 23:22:15 -0500

    Se creo la funcionalidad para agregar marcadores cuando hacemos las busquedas y se borran reinician cuando hacemos otras busquedas o no ponemos nada

--- src/context/map/MapProvider.tsx
+++ src/context/map/MapProvider.tsx
@@ -1,16 +1,19 @@
 import { Map, Marker, Popup } from 'mapbox-gl';
 import { MapContext } from './MapContext';
-import { useReducer } from 'react';
+import { useContext, useEffect, useReducer } from 'react';
 import { mapReducer } from './MapReducer';
+import { PlacesContext } from '../places/PlacesContext';
 
 export interface MapState {
     isMapReady: boolean,
     map?: Map
+    markers: Marker[],
 }
 
 const INITIAL_STATE: MapState = {
     isMapReady: false,
     map: undefined,
+    markers: []
 };
 
 interface Props {
@@ -20,6 +23,38 @@ interface Props {
 export const MapProvider = ({children}: Props) => {
 
     const [state, dispatch] = useReducer(mapReducer, INITIAL_STATE);
+
+    const { places } = useContext(PlacesContext);
+
+    useEffect(() => {
+        state.markers.forEach(marker => marker.remove());
+        const newMarkers: Marker[] = [];
+
+        for (const place of places) {
+
+            const [lng, lat] = place.center;
+            
+            const popup = new Popup()
+                .setHTML(`
+                    <h6>${place.text_es}</h6>
+                    <p>${place.place_name_es}</p>
+                `);
+                
+            const newMarker = new Marker()
+                .setPopup(popup)
+                .setLngLat([lng, lat])
+                .addTo(state.map!);
+                
+            newMarkers.push(newMarker);
+        }
+
+        //Todo Limpiar polyline
+
+        dispatch({type: 'setMarkers', payload: newMarkers});
+
+        console.log(places);
+
+    }, [places]);
 
     const setMap = (map: Map) => {
 
